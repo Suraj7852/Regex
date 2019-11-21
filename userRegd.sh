b7e384dd40a6c53d411d15ca49d1f007b2cc7c0e
Suraj7852 <surajpj7852@gmail.com>
2019-11-21 11:39:18 +0530

    ADDED: validate Mobile Number

--- userRegd.sh
+++ userRegd.sh
@@ -9,7 +9,9 @@ firstName=^[A-Z][a-z]{3,}
 lastName=^[A-Z][a-z]{3,}
 
 email="^(\w+)([+_-.]\w+)?[@][a-zA-Z]+([.][a-z]{2,3})([.][a-z]{2})?$"
-if [[ $registration =~ $email ]];
+
+mobNo="^[0-9]{2}([ ])?[0-9]{10}"
+if [[ $registration =~ $mobNo ]];
 then
        echo "yes"
 else
