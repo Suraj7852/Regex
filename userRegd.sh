46838319be61d85ba9ffd9032b1941ec60d2f3b7
Suraj7852 <surajpj7852@gmail.com>
2019-11-21 11:56:47 +0530

    ADDED: validate password as rule2

--- userRegd.sh
+++ userRegd.sh
@@ -10,10 +10,12 @@ lastName=^[A-Z][a-z]{3,}
 
 email="^(\w+)([+_-.]\w+)?[@][a-zA-Z]+([.][a-z]{2,3})([.][a-z]{2})?$"
 
-mobNo="^[0-9]{2}([ ])?[0-9]{10}"
+mobNo="^[0-9]{2}([ ])?[6-9]{10}"
 
 passwordRule1="\w{8,}"
-if [[ $registration =~ $passwordRule1 ]];
+
+passwordRule2="\w[A-Z]+"
+if [[ $registration =~ $passwordRule2 ]];
 then
        echo "yes"
 else
