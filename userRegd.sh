255a53adc968a7bf21d6cc88f981fa75744e6eb2
Suraj7852 <surajpj7852@gmail.com>
2019-11-21 11:35:44 +0530

    ADDED: validate email

--- userRegd.sh
+++ userRegd.sh
@@ -7,7 +7,9 @@ read registration
 firstName=^[A-Z][a-z]{3,}
 
 lastName=^[A-Z][a-z]{3,}
-if [[ $registration =~ $lastName ]];
+
+email="^(\w+)([+_-.]\w+)?[@][a-zA-Z]+([.][a-z]{2,3})([.][a-z]{2})?$"
+if [[ $registration =~ $email ]];
 then
        echo "yes"
 else
